2c0490086df6e92d58c9535323090d8053a2b375
“Jane <ttran161@ucsc.edu>
2022-04-10 23:17:53 -0700

    update

--- RocketPatrol/src/prefabs/Rocket.js
+++ RocketPatrol/src/prefabs/Rocket.js
@@ -6,4 +6,28 @@ class Rocket extends Phaser.GameObjects.Sprite {
       // add object to existing scene
       scene.add.existing(this);
     }
+
+    update() {
+      // left/right movement
+      if(!this.isFiring) {
+          if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+              this.x -= this.moveSpeed;
+          } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+              this.x += this.moveSpeed;
+          }
+      }
+      // fire button
+      if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+          this.isFiring = true;
+          this.sfxRocket.play();
+      }
+      // if fired, move up
+      if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+          this.y -= this.moveSpeed;
+      }
+      // reset on miss
+      if(this.y <= borderUISize * 3 + borderPadding) {
+          this.reset();
+      }
   }
+}
